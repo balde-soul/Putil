f6cead4b63e193a8c1b680a98dc10764fb47ba2e
blade <CJHdream@outlook.com>
2018-07-10 22:03:36 +0800

    fix the key error

--- np/util.py
+++ np/util.py
@@ -4,13 +4,13 @@ import numpy as np
 from Putil.tf import util
 
 _np_type = {
-    32.0: np.float32,
+    0.32: np.float32,
     32: np.int32,
-    64.0: np.float64,
+    0.64: np.float64,
     64: np.int64,
     8: np.uint8,
     -8: np.int8,
-    16.0: np.float16,
+    0.16: np.float16,
     16: np.int16
 }
 
--- tf/util.py
+++ tf/util.py
@@ -3,13 +3,13 @@ import tensorflow as tf
 from Putil.np import util
 
 _tf_type = {
-    32.0: tf.float32,
+    0.32: tf.float32,
     32: tf.int32,
-    64.0: tf.float64,
+    0.64: tf.float64,
     64: tf.int64,
     8: tf.uint8,
     -8: tf.int8,
-    16.0: tf.float16,
+    0.16: tf.float16,
     16: tf.int16
 }
 
