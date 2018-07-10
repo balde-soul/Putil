13710a8a6db26abc17c58d132187f9eaf465cd69
blade <CJHdream@outlook.com>
2018-07-10 20:08:24 +0800

    conmplete make np_type class

--- np/util.py
+++ np/util.py
@@ -1,1 +1,30 @@
 # coding = utf-8
+
+import numpy as np
+from Putil.tf import util
+
+_np_type = {
+    32.0: np.float32,
+    32: np.int32,
+    64.0: np.float64,
+    64: np.int64,
+    8: np.uint8,
+    -8: np.int8,
+    16.0: np.float16,
+    16: np.int16
+}
+
+
+class np_type:
+    def __init__(self, label):
+        self._type = _np_type[label]
+        self._label = label
+        pass
+
+    @property
+    def Type(self):
+        return self._type
+
+    def to_tf(self):
+        return util._tf_type[self._label]
+        pass
