076c38e6ee021c0f7b411ffc1183f39a15dc14de
blade <CJHdream@outlook.com>
2018-07-10 20:08:39 +0800

    complete make tf_type class

--- tf/util.py
+++ tf/util.py
@@ -1,7 +1,8 @@
 # coding = utf-8
 import tensorflow as tf
+from Putil.np import util
 
-tf_type = {
+_tf_type = {
     32.0: tf.float32,
     32: tf.int32,
     64.0: tf.float64,
@@ -11,3 +12,17 @@ tf_type = {
     16.0: tf.float16,
     16: tf.int16
 }
+
+
+class tf_type:
+    def __init__(self, label):
+        self._type = _tf_type[label]
+        self._label = label
+        pass
+
+    @property
+    def Type(self):
+        return self._type
+
+    def to_np(self):
+        return util._np_type[self._label]
