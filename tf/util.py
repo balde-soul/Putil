e1b508bd3ba7e3f86ad5a9d9e5567f4ed0c23cbe
blade <CJHdream@outlook.com>
2018-07-10 19:07:31 +0800

    add tensorflow type reflect tf_type

--- tf/util.py
+++ tf/util.py
@@ -1,1 +1,13 @@
 # coding = utf-8
+import tensorflow as tf
+
+tf_type = {
+    32.0: tf.float32,
+    32: tf.int32,
+    64.0: tf.float64,
+    64: tf.int64,
+    8: tf.uint8,
+    -8: tf.int8,
+    16.0: tf.float16,
+    16: tf.int16
+}
