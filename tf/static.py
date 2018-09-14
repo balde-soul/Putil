7a2f3eb54ad0a6e74cddaf0ea5c08602fbec5818
blade <CJHdream@outlook.com>
2018-09-14 10:51:52 +0800

    complement Paramprobe

--- tf/static.py
+++ tf/static.py
@@ -1,15 +1,26 @@
 # coding = utf-8
 import tensorflow as tf
 import warnings
-import logging
+import Putil.loger as plog
+import copy
+from colorama import Fore, Back, Style
+import json
+import threading
+
+root_logger = plog.PutilLogConfig("tf/static").logger()
+root_logger.setLevel(plog.DEBUG)
+ParamProbeLogger = root_logger.getChild("ParamProbe")
+ParamProbeLogger.setLevel(plog.DEBUG)
+RegularizeExtractLogger = root_logger.getChild("RegularizeExtract")
+RegularizeExtractLogger.setLevel(plog.DEBUG)
 
 
 # todo: generate initialize method, coding
 def initialize(method, dtype):
     """
-    
-    :param type: 
-    :return: 
+
+    :param type:
+    :return:
     """
     init = tf.zeros_initializer(dtype) if method == 'zero' else None
     init = tf.ones_initializer(dtype=dtype) if method == 'one' else None
@@ -54,11 +65,215 @@ class padding_convert:
     @property
     def High(self):
         return self.high()
+
+    pass
+
+
+def ParamUnfitInfo(param_default, param_feed):
+    return 'supported param keys is: {0}\n' \
+           'but input param keys are: {1}'.format(param_default.keys(), param_feed.keys())
+    pass
+
+
+def _DictPrint(_dict, _logger):
+    info = json.dumps(_dict, indent=2)
+    _logger.debug(info)
+    pass
+
+
+def DictPrintFormat(_dict):
+    info = json.dumps(_dict, indent=2)
+    return info
+
+
+def CheckKeys(_param_default, _param_feed):
+    """
+
+    :param _param_default: standard param dict
+    :param _param_feed: param wanted to used
+    :return: raise KeyError and print unfitted information in logging.INFO
+    """
+    for i in _param_feed.keys():
+        assert i in _param_default.keys(), \
+            ParamProbeLogger.info(Fore.RED + "error key: {0}\n{1}".format(i[0], ParamUnfitInfo(
+                _param_default, _param_feed)))
+        if type(i[1]).__name__ == 'dict':
+            pass
+        pass
+    pass
+
+
+"""
+class for fit the param request using the param we have now
+param generated dose not influence the param feed in
+usage:
+
+"""
+
+
+class ParamProbe:
+    def __init__(self, param_default, param_feed):
+        """
+
+        :param param_default: default param construct
+        :param param_feed: param now we have
+        """
+        self._param_default_set = threading.Lock()
+        # default param
+        self._param_default = param_default
+        # param feed in keep no change
+        self._param_feed = param_feed
+        # the param we deal with
+        # what we want to get
+        self._param_gen = param_feed.copy()
+        self._complement = ''
+        self._complement_default = ''
+        pass
+
+    def SetDefault(self, param_feed):
+        """
+        set the _param_default
+        :param param_feed:
+        :return:
+        """
+        self._param_default_set.acquire(blocking=True)
+        self._param_default = copy.deepcopy(param_feed)
+        self._param_default_set.release()
+        return self
+        pass
+
+    def UseDefault(self):
+        """
+        set _param_gen use the same as _param_default
+        :return:
+        """
+        self._param_gen = self._param_default
+        return self
+        pass
+
+    def ShowDefault(self, logger):
+        """
+        display param_default in Logger.debug
+        :param Logger:
+        :return:
+        """
+        _DictPrint(self._param_default, logger)
+        return self
+        pass
+
+    @staticmethod
+    def integrity_info():
+        # debug log
+        ParamProbeLogger.debug()
+        pass
+
+    def complement(self, **options):
+        """
+        complement the _param_gen using the special param in options
+        :param options:
+            key=value
+            key should be in the default param keys
+            value should be up to the mustard
+        :return:
+        """
+        for i in options.items():
+            self._param_gen[i[0]] = copy.deepcopy(i[1])
+            self._complement += ':{0}'.format(i)
+            self._complement += ']'
+            pass
+        return self
+
+    # : fix the param with the default param, if the param leaked fill it
+    def fix_with_default(self):
+        """
+        fix the param_gen using the param_default
+        while the param_gen missing parts in the param_default
+        just copy from param_default
+        :return:
+        """
+        for i in self._param_default.keys():
+            self._complement_default += '-'
+            if type(self._param_default[i]).__name__ == 'dict':
+                if self._param_gen.get(i, None) is None:
+                    self._param_gen[i] = self._param_default[i]
+                    self._complement_default += ':{0}'.format(i)
+                else:
+                    self._complement_default += ':{0}'.format(i)
+                    temp = ParamProbe(self._param_default[i], self._param_gen[i])
+                    self._param_gen[i] = temp.fix_with_default().ParamGen
+                    self._complement_default += '{0}'.format(temp._complement_default)
+                    pass
+                pass
+            else:
+                try:
+                    self._param_gen[i]
+                except KeyError:
+                    self._param_gen[i] = self._param_default[i]
+                    self._complement_default += ':{0}'.format(i)
+                    pass
+                pass
+            self._complement_default += ']'
+            pass
+        return self
+        pass
+
+    @property
+    def Default(self):
+        return self._param_default
+
+    @property
+    def ParamFeed(self):
+        return self._param_feed
+
+    @property
+    def ParamGen(self):
+        return self._param_gen
+
+    def ParamGenWithInfo(self, logger):
+        """
+        info unscramble:
+            display: :a]:b:e:f]:g]:h:i]]]:d]:c]], means:
+            every ']' means one level out
+            default:
+            {'a': [1, 2, 3], 'b': {'c': 'sdsd', 'd': False, 'e': {'f': 1.0, 'g': [1, 2], 'h': {'i': 1}}}}
+            param_feed:
+            {'b': {'e': {'h': {}}}}
+            would create this display
+            :a(first level)](a element out):b(level=a):e(level=b+1):f(level=e+1)](f out)
+            :g(level=f](g out)]:h(level=g):i(level=h+1)](i out)](h out)](e out):d(level=e)](d out)
+            :c(level=d)](c out)](level=b)
+            watch information we can make graph:
+            a   =   b
+            |       |
+                    e   =   d   =   c
+                    |
+                    f   =   g   =   h
+                                    |
+                                    i
+            and the leaky:
+                a/b:e:f/b:e:g/b:e:h/b:d/b:c
+        :param logger:
+        :return:
+        """
+        logger.info(Fore.YELLOW + 'use special complement:' + Style.RESET_ALL)
+        logger.info(Fore.GREEN + self._complement + Style.RESET_ALL)
+        logger.info(Fore.YELLOW + 'use default' + Style.RESET_ALL)
+        logger.info(Fore.GREEN + self._complement_default + Style.RESET_ALL)
+        return self._param_gen
+
+    @property
+    def ComplementDefault(self):
+        return self._complement_default
+
+    @property
+    def Complement(self):
+        return self._complement
     pass
 
 
 class initializer_extract:
     def __init__(self, param):
+
         self._param = param
         pass
 
@@ -69,6 +284,7 @@ class initializer_extract:
     def Param(self):
         return self._param
         pass
+
     pass
 
 
@@ -98,7 +314,8 @@ class regularize_extract:
             if key in self._default.keys():
                 pass
             else:
-                logging.warning('key: {0} is illegal, keys must in {1}'.format(key, self._default.keys()))
+                RegularizeExtractLogger.warning(
+                    'key: {0} is illegal, keys must in {1}'.format(key, self._default.keys()))
                 self._warn_up = True
         pass
 
@@ -119,27 +336,29 @@ class regularize_extract:
     @property
     def Default(self):
         return self._default
+
     pass
 
 
-class conv_extract:
+class conv_extract(ParamProbe):
     def __init__(self, param):
-        self._param = param
+        _default_param = {
+            "kernel": [3, 3],
+            "stride": [1, 1],
+            "padding": "SAME",
+            "WeightInitializer": {
+                "initializer": "mrsa",
+                "initializer_param": {
+                    "scale": 1.0,
+                    "mode": "fan_avg",
+                    "distribution": "normal",
+                    "seed": None,
+                    "dtype": 0.32
+                }
+            }
+        }
+        ParamProbe.__init__(self, _default_param, param)
         pass
-
-    def complement(self, **options):
-        self._param['kernel'] = options.pop('kernel', self._param('kernel', [3, 3]))
-        self._param['stride'] = options.pop('stride', self._param.get('stride', [1, 1]))
-        self._param['padding'] = options.pop('padding', self._param.get('padding', 'SAME'))
-        self._param['weight_initializer'] = \
-            options.pop('weight_initializer',
-                        self._param.get('weight_initializer',
-                                        initializer_extract({}).complement().Param))
-        self._param['weight_regularize'] = \
-            options.pop('weight_regularize', self._param.get('weight_regularize',
-                                                             regularize_extract({}).complement().Param))
-
-        return self
 
     @property
     def Param(self):
@@ -153,3 +372,4 @@ class conv_extract:
         return self._debug_string()
 
     pass
+
