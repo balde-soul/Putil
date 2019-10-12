36107253b9c866d0a67cbaee56a2671f86de9ba4
balde-soul <CJHdream@outlook.com>
2019-10-12 10:35:56 +0800

    add data test

---
+++ test/data/test_common_data.py
@@ -0,0 +1,77 @@
+# coding=utf-8
+import Putil.base.logger as plog
+
+plog.PutilLogConfig.config_log_level(stream=plog.DEBUG)
+plog.PutilLogConfig.config_handler(plog.stream_method)
+logger = plog.PutilLogConfig('TesCommonData').logger()
+logger.setLevel(plog.DEBUG)
+MainLogger = logger.getChild('Main')
+MainLogger.setLevel(plog.DEBUG)
+
+import Putil.data.common_data as pcd
+import multiprocessing
+
+
+class TestCommonData(pcd.CommonData):
+    def __init__(self):
+        pcd.CommonData.__init__(self)
+        self._restart_process()
+        pass
+
+    def _restart_process(self, restart_param=None):
+        self._field = list(range(0, 100))
+        pass
+
+    def _generate_from_one_sample(self):
+        target = self._field.pop()
+        return target
+        pass
+
+    def _status_update(self):
+        self._epoch_done = True if len(self._field) == 0 else False
+        pass
+    pass
+
+
+pcd.CommonDataManager.register('TestCommonData', TestCommonData, proxytype=pcd.CommonDataProxy)
+
+
+if __name__ == '__main__':
+    manager_common_data = pcd.CommonDataManager()
+    manager_common_data.start()
+    data = manager_common_data.TestCommonData()
+
+    manager = multiprocessing.Manager()
+    pool = multiprocessing.Pool()
+
+    restart_param = manager.dict()
+    dpq = pcd.DataPutProcess(data, manager, pool)
+    pool.close()
+
+    dq = dpq.DataQueue
+
+    # pool.join()
+    # print(dpq.queue_process_ret.get())
+
+    dpq.restart(restart_param)
+    count = 0
+    while dpq.has_next:
+        # print('s')
+        dq.get()
+        count += 1
+        pass
+    assert count == 100
+    dpq.restart(restart_param)
+    count = 0
+    while dpq.has_next:
+        dq.get()
+        count += 1
+        pass
+    assert count == 100
+    dpq.stop_generation()
+    pool.join()
+    print(dpq.queue_process_ret.get())
+    # while dq.empty() is False or dpq.EpochDoneFlag.value is False:
+    #     print('get')
+    #     print(dq.get())
+    pass
---
+++ test/data/test_ilsvrc_data.py
@@ -0,0 +1,11 @@
+# coding=utf-8
+import Putil.data.ilsvrc_data as ilsvrcd
+import 
+
+
+def base_test():
+    pass
+
+
+if __name__ == '__main__':
+    pass
