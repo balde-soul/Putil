628dce299f27992a9f9bf0bbb2fbfd66c0b80644
balde-soul <CJHdream@outlook.com>
2019-10-16 21:54:10 +0800

    add coco data unit test, developping

---
+++ test/data/test_coco_data_unit.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+impor Putil.base.logger as plog
+
+plog.PutilLogConfig.config_handler(plog.stream_method)
+plog.PutilLogConfig.config_log_level(stream=plog.DEBUG)
+plog.PutilLogConfig.config_format(plog.Format)
+
+
+logger = plog.PutilLogConfig('TestCOCODataUnit').logger()
+logger.setLevel(plog.DEBUG)
+
+import Putil.data.coco as coco
+
+coco.COCOData.statistic()
