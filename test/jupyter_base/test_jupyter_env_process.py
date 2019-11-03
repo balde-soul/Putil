2f222e57808f4bc1c464677c4c2d10319c9718e4
balde-soul <CJHdream@outlook.com>
2019-11-04 07:57:13 +0800

    for sync

--- test/jupyter_base/test_jupyter_env_process.py
+++ test/jupyter_base/test_jupyter_env_process.py
@@ -1,11 +1,17 @@
 # coding=utf-8
+
 import sys
+import os
 
 print(sys.path)
 import Putil.jupyter_base.jupyter_env_process as jep
 
-jep.add_jupyter_notbook_path_to_env('/home/sins/Download/')
+jep.add_jupyter_notbook_path_to_env(os.path.join(os.getcwd(), 'test/jupyter_base/for_jupyter_import_test_code'))
 
 print(sys.path)
 
-import Cloud.cloud2 as cloud
+import sub.for_jupyter_import_test as sfjit
+sfjit.test()
+
+import for_jupyter_import_test as fjit
+fjit.test()
