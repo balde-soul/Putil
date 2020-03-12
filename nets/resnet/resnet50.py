5930560a16e7b09288188fabf776ad8d97228040
balde-soul <CJHdream@outlook.com>
2020-03-12 20:48:56 +0800

    add nets

---
+++ nets/resnet/resnet50.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+import tensorflow as tf
+import slim
+
+
+class resnet50:
+    def __init__(self):
+        pass
+
+    def load_weight(self, checkpoint_file='', weights=None):
+        pass
+
+    def resnet_weights(self):
+        '''
+        this function return the weights of resnet
+        '''
+        pass
+
+    def resnet_resolution_output(self, resolution=None):
+        '''
+        this function return the output in different resolution
+        '''
+        pass
+    pass
