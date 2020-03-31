f474caac123952df5a79679cdae4b731eecab75c
balde-soul <CJHdream@outlook.com>
2020-03-31 08:34:23 +0800

    add default parameter illustration

--- trainer/auto_stop.py
+++ trainer/auto_stop.py
@@ -36,10 +36,10 @@ class AutoStop:
     @staticmethod
     def generate_args(parser):
         parser.add_argument('--auto_stop_patience', dest='AutoStopPatience', type=int, action='store', default=8, 
-        help='when the time count to patience, it would stop')
+        help='when the time count to patience, it would stop, default: 8')
         parser.add_argument('--auto_stop_mode', dest='AutoStopMode', type=str, action='store', default='max', 
         help='max or min, the change meaning the indicator is better, if the change do not match the mode, \
-        we would cound the time')
+        we would cound the time, default: max')
         pass
 
     @staticmethod
