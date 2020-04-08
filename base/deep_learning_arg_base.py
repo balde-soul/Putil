5c2b5e1d0573d931faa673b63c13fa89b16ad211
balde-soul <CJHdream@outlook.com>
2020-04-08 20:24:00 +0800

    add deep_learning_arg_base

---
+++ base/deep_learning_arg_base.py
@@ -0,0 +1,36 @@
+# coding=utf-8
+# this file is call before logger config
+# Putil
+import base.arg_base as pab 
+
+
+class SummaryManager:
+    def __init__(self, summary_frequent):
+        pass
+
+    def summary_or_not(self, epoch, step, evaluate, train):
+        pass
+    pass
+
+class DLBaseArgs(ppb.ProjectArg):
+    def __init__(self, parser=None, train=True, evaluate=True, test=True, *args, **kwargs):
+        pab.ProjectArg.__init__(self, parser, args, kwargs)
+        self._parser.add_argument('--device', dest='DLBaseArgsDevice', type=list, action='store', default=[0], 
+        help='the device (list) used in tensorflow model runing, use DLBaseArgsDevice to get the arg')
+        self._parser.add_argument('--batch_size', dest='DLBaseArgsBatchSize', type=list, action='store', default=[16],
+        help='the batch size (list) for every device, use DLBaseArgsBatchSize to get the arg')
+        self._parser.add_argument('--summary_frequent', dest='DLBaseArgsSummaryFrequent', type=int, action='store', default=32,
+        help='the frequent base on the batch for make summary')
+        if train:
+            self._parser.add_argument('--epoch', dest='DLBaseArgsEpoch', type=int, action='store', 
+            help='the training(int) epoch, use DLBaseArgsEpoch to get the arg')
+            pass
+        if evaluate:
+            self._parser.add_argument('--evaluate_frequent', dest='DLBaseArgEvaluateRate', type=int, action='store',
+            help='the frequent(int) base on train epoch for evaluating, use DLBaseArgEvaluateRate to get the arg')
+            pass
+        if test:
+            self._parser.add_argument('--test', dest='TF')
+            pass
+        pass
+    pass
