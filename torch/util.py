8e13f9274e5e113a629cef2c8dbff84f7c11884b
balde-soul <CJHdream@outlook.com>
2020-06-30 09:22:43 +0800

    add the torch_deterministic

--- torch/util.py
+++ torch/util.py
@@ -1,1 +1,12 @@
-# cod
+# coding=utf-8
+
+def set_torch_deterministic(seed):
+    import torch
+    import numpy as np
+    import random
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    pass
