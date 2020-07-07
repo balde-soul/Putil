8d36fdb015135133442bbdc57c365c0bbf670a7f
balde-soul <CJHdream@outlook.com>
2020-07-07 20:21:50 +0800

    bbox_convertor:1

--- data/vision_common_convert/bbox_convertor.py
+++ data/vision_common_convert/bbox_convertor.py
@@ -25,7 +25,7 @@ class BBoxConvertToCenterBox(convert_to_input.ConvertToInput):
     def __init__(
         self, 
         sample_rate, 
-        sigma=np.array([[0.5, 0.0], [0.0, 0.5]], dtype=np.float32), 
+        sigma=np.array([[3.0, 0.0], [0.0, 3.0]], dtype=np.float32), 
         mu=np.array([[0.0], [0.0]], dtype=np.float32)):
         convert_to_input.ConvertToInput.__init__(self)
         self._sample_rate = sample_rate
@@ -44,7 +44,7 @@ class BBoxConvertToCenterBox(convert_to_input.ConvertToInput):
         '''
         image = args[0]
         boxes = args[1]
-        label = np.zeros(shape=[image.shape[0] // self._sample_rate, image.shape[1] // self._sample_rate, 4], dtype=np.float32)
+        label = np.zeros(shape=[image.shape[0] // self._sample_rate, image.shape[1] // self._sample_rate, 7], dtype=np.float32)
         temp_weight = np.zeros(shape=[image.shape[0], image.shape[1], 4], dtype=np.float32) 
         for box in boxes:
             x_cell_index = (box[0] + 0.5) // self._sample_rate
@@ -59,12 +59,15 @@ class BBoxConvertToCenterBox(convert_to_input.ConvertToInput):
             yregion = [round(box[1] - box[3] * 0.5), round(box[1] + box[3] * 0.5)]
             x = np.linspace(-(xregion[1] - xregion[0]) * 0.5, (xregion[1] - xregion[0]) * 0.5, num=xregion[1] - xregion[0] + 1)
             y = np.linspace(-(yregion[1] - yregion[0]) * 0.5, (yregion[1] - yregion[0]) * 0.5, num=yregion[1] - yregion[0] + 1)
-            coor = np.meshgrid(x, y)
-            coor = np.reshape(np.stack(coor, axis=-1), [-1, 2])
-            weights = self._weight_func.calc(coor)
-            label[y_cell_index][x_cell_index][0] = 1.0
-            label[y_cell_index][x_cell_index][1: 5] = [x_cell_shift, y_cell_shift, w_cell, h_cell]
-        return args
+            x, y = np.meshgrid(x, y)
+            shape = x.shape
+            coor = np.reshape(np.stack([x, y], axis=-1), [-1, 2])
+            weights = self._weight_func(coor)
+            weights = np.reshape(weights, shape)
+            #label[y_cell_index][x_cell_index][0] = 1.0
+            #label[y_cell_index][x_cell_index][1: 5] = [x_cell_shift, y_cell_shift, w_cell, h_cell]
+            #label[xregion[0]: xregion[1]][yregion[0]: yregion[1]][6] = weights
+        return image, label, weights
 ##In[]
 #from PIL import Image
 #import numpy as np
--- test/data/vision_common_convert/test_bbox_convertor.py
+++ test/data/vision_common_convert/test_bbox_convertor.py
@@ -1,9 +1,16 @@
 # coding=utf-8
+#In[]:
 import Putil.data.vision_common_convert.bbox_convertor as bbc
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 BC = bbc.BBoxConvertToCenterBox(sample_rate=4)
 image = np.reshape(np.random.sample(10000), [100, 100])
 print(image.shape)
-BC(image, [[60, 50, 30, 50]])
+image, label, weight = BC(image, [[60, 50, 30, 80]])
+print(weight.shape)
+plt.imshow((weight * 255).astype(np.uint8), cmap=plt.cm.gray)
+plt.show()
+#plt.imshow((label[:, :, 6] * 255).astype(np.uint8))
+#plt.show()
