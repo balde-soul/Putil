40b9c229fd2d0e2556d1ef344ac94ff984417fd7
blade-soul <CJHdream@outlook.com>
2020-07-31 17:22:51 +0800

    update

--- test/trainer/test_multiprocess_auto_save.py
+++ test/trainer/test_multiprocess_auto_save.py
@@ -2,17 +2,40 @@ import multiprocessing as mp
 import Putil.trainer.multiprocess_auto_save as mas
 from multiprocessing.managers import BaseManager
 
+class TM(BaseManager):
+    pass
 
-if __name__ == 'main':
-    BaseManager.register('T', mas.T)
-    manager = BaseManager(address=('127.0.0.1', 4000), authkey=b'test')
+TM.register('T', mas.T)
+print(__name__)
+
+
+if __name__ == '__main__':
+
+    mp.set_start_method('spawn')
+    #tm = TM()
+    #tm.start()
+    #t = tm.T()
+    t = 0
+    
+    BaseManager.register('get_t', callable=lambda: t)
+    manager = BaseManager(address=('', 18888), authkey=b'test')
     manager.start()
-
-    t = manager.T()
+else:
+    BaseManager.register('get_t')
+    manager = BaseManager(address=('127.0.0.1', 18888), authkey=b'test')
+    manager.connect()
+    pass
 
 def main():
-    mp.spawn()
-    mp.spawn.spawn_main()
+    #mp.Process(target=master)
+    pass
+
+def master():
+    t = manager.get_t()
+    print(t)
+    pass
+
+def worker():
     pass
 
 
