702600f7c571953ddd5eb8e98b4afa773b33559b
jihuacao <CJHdream@outlook.com>
2023-12-25 21:32:04 +0800

    add sampling

--- test/sampling/MCMC/test_metropolis_hasting.py
+++ test/sampling/MCMC/test_metropolis_hasting.py
@@ -29,3 +29,25 @@ plt.show()
 plt.contourf(X, Y, Z, 100, alpha=1.0, cmap=cm.coolwarm)
 plt.scatter(xs[:, 0], xs[:, 1], c='g', marker='.', linewidths=0.001)
 plt.show()
+
+def rf():
+    #return [np.random.choice(ss, size=1), np.random.choice(ss, size=1)]
+    return [np.random.random() * 2 - 1, np.random.random() * 2 - 1]
+def pdff(x):
+    return 1 / (2 * np.pi * np.sqrt(1 - 0.85)) * np.exp(1 / (2 * (1 - 0.85)) * (- x[0]**2 - x[1]**2) + 2 * 0.85 * x[0] * x[1])
+ss = np.linspace(-1, 1, 100)
+a = mh.MetropolisHasting()
+a.set_random_func(rf)
+a.set_pdf_func(pdff)
+xs = list()
+for i in range(0, 20000):
+    xs.append(a.sample())
+    pass
+xs = np.array(xs)
+X, Y = np.meshgrid(ss, ss)
+Z = pdff([X, Y])
+plt.contourf(X, Y, Z, 100, alpha=1.0, cmap=cm.coolwarm)
+plt.show()
+plt.contourf(X, Y, Z, 100, alpha=1.0, cmap=cm.coolwarm)
+plt.scatter(xs[:, 0], xs[:, 1], c='g', marker='.', linewidths=0.001)
+plt.show()
